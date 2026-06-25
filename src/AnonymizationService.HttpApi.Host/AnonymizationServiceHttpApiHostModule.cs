381a77d70ab4005b6e925d378f5af827c8b6db1b
Francesco Tomo <francesco.tomo@dgsspa.com>
2026-06-25 13:09:18 +0200

    Aggiunto riferimento a Volo.Abp.Automapper v10.4.1 al progetto Application In "AnonymizationServiceHttpApiHostModule" del progetto Host modificato using Microsoft.OpenApi.Models in using Microsoft.OpenApi;

--- src/AnonymizationService.HttpApi.Host/AnonymizationServiceHttpApiHostModule.cs
+++ src/AnonymizationService.HttpApi.Host/AnonymizationServiceHttpApiHostModule.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;//.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
